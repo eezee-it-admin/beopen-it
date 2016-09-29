124b86ff3532afbf2bb230f9ed56843fc56b0e13
huysmki <kim.huysmans@rhea.be>
2016-09-29 16:26:33 +0200

    initial commit of full repository

---
+++ beopen_addons/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Beopen addons",
+
+    'summary': """Beopen addons""",
+
+    'description': """
+        Beopen addons
+    """,
+    'depends': ['web', "base"],
+    'auto_install': True,
+    'author': "BeOpen NV",
+    'website': "http://www.beopen.be",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Addons',
+    'version': '0.1',
+    # always loaded
+    'data': [
+        'views/template.xml',
+    ],
+}
---
+++ template_configurator/__openerp__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "BeOpen Template Configurator",
+
+    'summary': """BeOpen Template Configurator""",
+
+    'description': """
+        BeOpen Template Configurator
+    """,
+
+    'author': "Rhea NV",
+    'website': "http://www.rhea.be",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Test',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['website', 'mail', 'crm'],
+
+    # always loaded
+    'data': [
+ #       'security/security.xml',
+        'security/ir.model.access.csv',
+ #       'templates.xml',
+        'views/template_configurator.xml',
+        'views/view_configurator_for_code.xml',
+        'views/view_module_prices.xml',
+        'data/data.xml',
+ #       'views/session_board.xml',
+ #       'reports.xml',
+    ],
+    # only loaded in demonstration mode
+#    'demo': [
+#        'demo.xml',
+#    ],
+}
