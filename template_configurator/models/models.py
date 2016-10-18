4e81b72ec2bce718bb791be1412eb7be8e1ffdcd
jo.olluyn@cronos.be <jo.olluyn@cronos.be>
2016-10-18 09:04:28 +0200

    Modelaanpassingen voor configuratie Docker

--- template_configurator/models/models.py
+++ template_configurator/models/models.py
@@ -1,5 +1,6 @@
 from openerp import models, fields, api, tools
 
+
 class Market(models.Model):
     _name="botc.market"
 
@@ -10,6 +11,28 @@ class Goal(models.Model):
     _name="botc.goal"
 
     name=fields.Char(string="Type", required=True)
+
+class Flavor(models.Model):
+    _name="botc.flavor"
+
+    name=fields.Char(String="Flavor")
+    version=fields.Float(String="Version", digits=(2, 1))
+    edition=fields.Char(String="Edition")
+
+class DbServer(models.Model):
+    _name="botc.dbserver"
+
+    name=fields.Char(string="Name", required=True)
+    ip=fields.Char(string="IP", required=True)
+    port=fields.Integer(string="Port", required=True)
+    username=fields.Char(string="Username", required=True)
+    pwd=fields.Char(string="Password", required=True)
+
+class DockerServer(models.Model):
+    _name="botc.dockerserver"
+
+    name=fields.Char(string="Name", required=True)
+    ip=fields.Char(string="IP", required=True)
 
 
 class MarketType(models.Model):
@@ -50,6 +73,9 @@ class Module(models.Model):
     module_ids=fields.Many2many("botc.module", "botc_module_module_dependency", "module_id", "dependent_module_id", string="Dependent Modules")
     odoo_module_name=fields.Char(string="Odoo Module Name", required=True)
 
+    standard=fields.Boolean(string="Standard", default=True)
+    package_file_location=fields.Char(string="Package File Location")
+
 class AvailableModules(models.Model):
     _name="botc.availablemodules"
     _rec_name = "module_id"
@@ -57,4 +83,70 @@ class AvailableModules(models.Model):
     module_id=fields.Many2one("botc.module", string="Module", required=True)
     markettype_id = fields.Many2one("botc.markettype", string="Market Type", required=True)
     included = fields.Boolean(string="Included in package", default=False)
+    flavor_id=fields.Many2one("botc.flavor", string="Flavor")
     order = fields.Integer(string="Sort Order")
+
+class DockerImage(models.Model):
+    _name="botc.dockerimage"
+
+    name=fields.Char(string="Name", required=True)
+    image_name=fields.Char(String="Image Name", required=True)
+    flavor_id=fields.Many2one("botc.flavor", string="Flavor")
+    volume_ids=fields.One2many("botc.volume", "docker_image_id", "Volumes")
+    port_ids = fields.One2many("botc.port", "docker_image_id", "Ports")
+
+
+class Volume(models.Model):
+    _name = "botc.volume"
+
+    name = fields.Char(string="Name", required=True)
+    path = fields.Char(string="Path", required=True)
+    docker_image_id = fields.Many2one("botc.dockerimage", string="Docker Image", required=True)
+
+
+class Port(models.Model):
+    _name = "botc.port"
+    _rec_name = "portnumber"
+
+    portnumber = fields.Integer(string="Portnumber", required=True)
+    docker_image_id = fields.Many2one("botc.dockerimage", string="Docker Image", required=True)
+
+class InstanceModule(models.Model):
+    _name="botc.instancemodule"
+    _rec_name="container_instance_id"
+
+    container_instance_id=fields.Many2one("botc.containerinstance", string="Container Instance", required=True)
+    module_id=fields.Many2one("botc.module", string="Module", required=True)
+    installed_on = fields.Datetime(string="Installed on")
+    uninstalled_on = fields.Datetime(string="Uninstalled on")
+
+class ContainerInstance(models.Model):
+    _name="botc.containerinstance"
+    _rec_name="domain"
+
+    domain=fields.Char(string="Domain", required=True)
+    market_type_id=fields.Many2one("botc.markettype", string="Market Type", required="True")
+    module_ids=fields.One2many("botc.instancemodule", "container_instance_id", "Modules")
+    dbserver_id=fields.Many2one("botc.dbserver", string="DB Server", required=True)
+    port_mapping_ids = fields.One2many("botc.portmapping", "container_instance_id")
+    volume_mapping_ids = fields.One2many("botc.volumemapping", "container_instance_id")
+    docker_image_id=fields.Many2one("botc.dockerimage", string="Docker Image")
+    docker_server_id=fields.Many2one("botc.dockerserver", string="Docker Server")
+    flavor=fields.Char(string="Flavor", related="docker_image_id.flavor_id.name", readonly=True)
+
+
+class PortMapping(models.Model):
+    _name="botc.portmapping"
+    _rec_name="container_instance_id"
+
+    container_instance_id=fields.Many2one("botc.containerinstance", string="Container Instance", required=True)
+    port_id=fields.Many2one("botc.port", string="Port")
+    port_map=fields.Integer(string="mapped to")
+
+class VolumeMapping(models.Model):
+    _name="botc.volumemapping"
+    _rec_name="container_instance_id"
+
+    container_instance_id=fields.Many2one("botc.containerinstance", string="Container Instance", required=True)
+    volume_id=fields.Many2one("botc.volume", string="Volume")
+    volume_map=fields.Char(string="mapped to")
